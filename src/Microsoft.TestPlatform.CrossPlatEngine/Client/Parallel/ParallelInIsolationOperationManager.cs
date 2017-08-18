30ad6ba741d19d5e80c02e2321a54e5e1dd413a6
faahmad <faahmad@FAAHMAD-DEV32>
2017-08-18 11:02:40 +0530

    Classes for parallelExecution in NoIsolation

---
+++ src/Microsoft.TestPlatform.CrossPlatEngine/Client/Parallel/ParallelInIsolationOperationManager.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.TestPlatform.CrossPlatEngine.Client.Parallel
+{
+    using System;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Engine;
+
+    internal abstract class ParallelInIsolationOperationManager<T, U> : IParallelOperationManager, IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateParallelLevel(int parallelLevel)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Microsoft.TestPlatform.CrossPlatEngine/Client/Parallel/ParallelNoIsolationProxyExecutionManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.VisualStudio.TestPlatform.CrossPlatEngine.Client.Parallel
+{
+    class ParallelNoIsolationProxyExecutionManager
+    {
+    }
+}
